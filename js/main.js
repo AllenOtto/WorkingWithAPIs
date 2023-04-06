29aac65887a12a4d11e9b4c4853f44969b9d2030
AllenOtto <kephamoturi@gmail.com>
2023-04-06 13:21:56 +0300

    asteroids/near earth objects api

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 
 // The Cocktail DB Drinks API (thecocktaildb.com>api)
 
-document.querySelector("button").addEventListener("click", getDrink)
+// document.querySelector("button").addEventListener("click", getDrink)
 
 function getDrink() {
     let someDrink = document.querySelector("input").value
@@ -25,15 +25,14 @@ function getDrink() {
     });
 }
 
-
-document.querySelector('button').addEventListener('click', getDrink)
+// document.querySelector('button').addEventListener('click', getDrink)
 
 function getDrink() {
     const drink = document.querySelector('input').value
 
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
         .then(res => res.json())
-        .then(data => {y
+        .then(data => {
             let count = Math.floor(Math.random() * data.drinks.length)
             console.log(data)
             document.querySelector('h2').innerText = data.drinks[count].strDrink
@@ -46,12 +45,10 @@ function getDrink() {
         });
 }
 
-getDrink()
-
 // NASA's Picture of the day API
 // =============================
 
-document.querySelector('button').addEventListener('click', apod);
+// document.querySelector('button').addEventListener('click', apod);
 
 function apod() {
     fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
@@ -68,6 +65,10 @@ function apod() {
         });
 }
 
+// Near Earth Objects
+
+// document.querySelector('button').addEventListener('click', nearEarthObjects);
+
 function nearEarthObjects() {
     fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=DEMO_KEY')
         .then(res => res.json())
@@ -80,8 +81,30 @@ function nearEarthObjects() {
         });
 }
 
+// Asteroid data
 
+document.querySelector('button').addEventListener('click', browseAsteroidData);
 
+function browseAsteroidData() {
+    fetch('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=DEMO_KEY')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            let rand = Math.floor(Math.random() * data.near_earth_objects.length)
+            document.querySelector("h2").innerText = data.near_earth_objects[rand].name;
+            document.querySelector("span").innerText = data.near_earth_objects[rand].close_approach_data[0].miss_distance.kilometers;
+            document.querySelector("p").innerText = data.near_earth_objects[rand].close_approach_data[0].relative_velocity.kilometers_per_second;
+        })
+        .catch(err => {
+            console.log("Error: " + err);
+        });
+}
+
+// Covid Injection Information
+
+function covidVaccinatInfo() {
+    fetch("https://api.nasa.gov/DONKI/CME?startDate=yyyy-MM-dd&endDate=yyyy-MM-dd&api_key=DEMO_KEY")
+}
 
 
 
